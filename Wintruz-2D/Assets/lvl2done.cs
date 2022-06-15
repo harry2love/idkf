3b7f84d6c8e6817279c498fb6557b44a756b9a1e
harry2love <hs405092@gmail.com>
2022-06-15 19:44:41 +0200

    ONE LAST TIIIIIIME

---
+++ Wintruz-2D/Assets/lvl2done.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class lvl2done : MonoBehaviour
+{
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.x > 45)
+        {
+            SceneManager.LoadScene("Level3");
+        }
+    }
+}
